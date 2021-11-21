ba6b9166681a0687a9e7341c21453eb65e8ffbbf
Alex-Verg <alexvergelyuk@gmail.com>
2021-11-21 17:52:22 +0200

    Finish johnson function. Change ford_bellman and dijkstra functions to use in johnson.

--- graphs.py
+++ graphs.py
@@ -16,16 +16,16 @@ class Graph():
 
     def __init__(self, table, v=30):
         self.matr_sub = copy.deepcopy(table)
-        self.v = 30
+        self.v = len(table)
 
-    def ford_bellman(self, sub=False):
+    def ford_bellman(self, sup=False):
         answ = [[0 if i == j else 10e7 for j in range(self.v)] for i in range(self.v)]
 
-        for k in range(self.v-1 if sub else 0, self.v):
+        for k in range(self.v-1 if sup else 0, self.v):
             for i in range(self.v-1):
                 for u in range(self.v):
                     for v in range(self.v):
-                        if (self.matr_sub[u][v] != 0 if not sub else True) and answ[k][v] > answ[k][u] + self.matr_sub[u][v]:
+                        if self.matr_sub[u][v] != 0  and answ[k][v] > answ[k][u] + self.matr_sub[u][v]:
                             answ[k][v] = answ[k][u] + self.matr_sub[u][v]
 
             if not k:
@@ -37,8 +37,9 @@ class Graph():
 
         return answ
 
-    def dijkstra(self):
+    def dijkstra(self, sup=False):
         answ = [[0 if i == j else 10e7 for j in range(self.v)] for i in range(self.v)]
+        eq = None if sup else 0
 
         for k in range(self.v):
             not_visit = [True for j in range(self.v)]
@@ -51,7 +52,7 @@ class Graph():
                     break
                 not_visit[v] = False
                 for e in range(self.v):
-                    if self.matr_sub[v][e] != 0 and answ[k][v] + self.matr_sub[v][e] < answ[k][e]:
+                    if self.matr_sub[v][e] != eq and answ[k][v] + self.matr_sub[v][e] < answ[k][e]:
                         answ[k][e] = answ[k][v] + self.matr_sub[v][e]
 
         return answ
@@ -83,15 +84,16 @@ class Graph():
         return A[-1]"""
 
     def johnson(self):
-        pass
-        """if not self.ford_bellman():
+
+        if not self.ford_bellman():
             return False
 
-        self.matr_sub.append([1] * 31)
+        self.matr_sub.append([1] * (self.v+1))
         for i in range(self.v):
             self.matr_sub[i].append(0)
         self.v += 1
-        h = self.ford_bellman(sub=True)[30][:-1]
+        h = self.ford_bellman(sup=True)[-1][:-1]
+        #print(*h)
         self.v -= 1
         for i in range(self.v):
             self.matr_sub[i].pop()
@@ -102,8 +104,17 @@ class Graph():
             for j in range(self.v):
                 if self.matr_sub[i][j] != 0:
                     self.matr_sub[i][j] = self.matr_sub[i][j] + h[i] - h[j]
+                elif (i != j):
+                    self.matr_sub[i][j] = None
+        
+        answ = self.dijkstra(sup=True)
+        for i in range(self.v):
+            for j in range(self.v):
+                answ[i][j] = answ[i][j] + h[j] - h[i]
 
-        """
+        self.matr_sub = tmp_w
+
+        return answ
 
 
 if __name__ == "__main__":
@@ -111,4 +122,9 @@ if __name__ == "__main__":
     print(*gr.ford_bellman())
     print(*gr.dijkstra())
     print(*gr.floyd_warshall())
-    print(gr.johnson())
+    print(*gr.johnson())
+
+    #gr = Graph([[0, -2, 7, 5], [0, 0, 8, 6], [0, 3, 0, -4], [-1, 0, 0, 0]])
+    #print(*gr.floyd_warshall())
+    #print(*gr.ford_bellman())
+    #print(*gr.johnson())
